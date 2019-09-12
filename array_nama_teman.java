effbf4751c84855e288253f335930dcce747957c
naufal025 <53498520+naufal025@users.noreply.github.com>
2019-09-12 19:19:02 +0700

    Add files via upload

---
+++ array_nama_teman.java
@@ -0,0 +1,17 @@
+
+package array;
+
+
+public class array_nama_teman {
+    public static void main(String[] args){
+        String [] temans = {"Nesya","Tata","Simon"};
+       System.out.println(" teman pertama :"+temans[0]+" teman kedua :" + temans[1] +" teman ketiga :"+temans[2]);
+        
+   
+       
+        
+        
+        
+    }
+    
+}
