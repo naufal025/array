f23fb25b14e6ef6f98478feee4485f4c734c8967
naufal025 <53498520+naufal025@users.noreply.github.com>
2019-09-12 19:20:34 +0700

    Add files via upload

---
+++ challenge2array.java
@@ -0,0 +1,39 @@
+
+public class challenge2array {
+    public static void main (String[] args){
+       int nilai [] = new int [5] ;
+       nilai [0] = 70;
+       nilai [1] = 60;
+       nilai [2] = 50;
+       nilai [3] = 90;
+       nilai [4] = 70;
+       double ratarata = 0.0;
+       for(int i=0; i<nilai.length; i++)ratarata+=nilai [i];
+        ratarata/=nilai.length;
+       
+       if (ratarata < 60){
+        System.out.println("Nilai rata-rata terkecil adalah :" +ratarata);
+        if (ratarata > 60){
+            
+        }
+       } else  System.out.println("Nilai rata-rata terbesar adalah :" +ratarata);
+       }
+    }
+
+         
+             
+         
+          
+      
+           
+       
+       
+           
+
+       
+       
+           
+       
+    
+    
+
